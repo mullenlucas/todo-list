ef968b47dda1ef8d3c210730f9c3544735673280
mullenslucas <mullenlucas@gmail.com>
2022-08-11 19:20:17 -0300

    develop classes, methods and html to list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
 import _ from 'lodash';
 import './style.css';
 
-// function component() {
-//   const element = document.createElement('div');
+// Individual Task class, regarding properties and methods for one task
+class Task {
+  constructor(desc,bval,index){
+    this.desc = desc,
+    this.bval = bval,
+    this.index = index
+  }
 
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('hello');
+  get taskDesc() {
+    return this.desc
+  }
 
-//   return element;
-// }
+  get taskBval() {
+    return this.bval
+  }
 
-// document.body.appendChild(component());
+  get taskIndex() {
+    return this.index
+  }
+}
 
-const tasks = [
+// Class that contains collection of tasks
+class Tasks {
+  constructor(){
+    this.tasks = []
+  }
+
+  // create a new task and save it in the collection using the Task class declared above
+  newTask(desc, bval, index){
+    let t = new Task(desc, bval,index)
+    this.tasks.push(t)
+    return t
+  }
+
+  get allTasks(){
+    return this.tasks
+  }
+}
+
+// Initiate Tasks class
+const tasksCl = new Tasks()
+
+// Load example tasks to the class
+const exampleTasks = [
   {
     description: "Complete a daily challenge",
     completed: true,
@@ -31,27 +62,40 @@ const tasks = [
   },
 ]
 
+exampleTasks.forEach((t) => {
+  tasksCl.newTask(t.description, t.completed, t.index)
+})
+
 const todoSec = document.querySelector('todo-sec')
-const todoUlDesc = document.getElementById('todo-ul-desc')
-const todoUlBool = document.getElementById('todo-ul-bool')
-let descHtml= ""
+const todoUl = document.getElementById('todo-ul')
+
 let t = ""
 
-tasks.map((task) => {
+tasksCl.allTasks.forEach((task) => {
   
-
-  task.completed ? t="Done" : t="Pending"
+  task.taskBval ? t="Done" : t="Pending"
   let liB = document.createElement('li')
+  liB.setAttribute('id', `id${task.taskIndex}`)
   let x = document.createElement("INPUT")
   x.setAttribute("type", "checkbox")
   liB.appendChild(x)
-  todoUlBool.appendChild(liB)
-  descHtml += `
-  <li>
-  ${task.description}
-  </li>
-  `
+  let p = document.createElement('p')
+  p.setAttribute('id', `p${task.taskIndex}`)
+  p.innerHTML = task.taskDesc
+  liB.appendChild(p)
+
+  let i = document.createElement('i')
+  i.setAttribute('class', 'fa-solid fa-ellipsis-vertical')
+  liB.appendChild(i)
+
+  todoUl.appendChild(liB)
 
 }
 )
-todoUlDesc.innerHTML = descHtml
+
+let liClear = document.createElement('li')
+liClear.setAttribute('id', 'clearList')
+let pClear = document.createElement('p')
+pClear.innerHTML = "Clear all completed"
+liClear.appendChild(pClear)
+todoUl.appendChild(liClear)
