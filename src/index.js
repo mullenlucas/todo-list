285c56295e75637ef88a61d36f00ae7a7b08f21e
mullenslucas <mullenlucas@gmail.com>
2022-08-11 22:05:40 -0300

    add functionality to add item to list

--- src/index.js
+++ src/index.js
@@ -1,50 +1,29 @@
 import './style.css';
 import Tasks from './modules/Tasks.js';
+
 // Initiate Tasks class
 const tasksCl = new Tasks();
 
-// Load example tasks to the class
-const exampleTasks = [
-  {
-    description: 'Complete a daily challenge',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Update and polish projects',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Respond e-mails',
-    completed: true,
-    index: 3,
-  },
-];
-
-exampleTasks.forEach((t) => {
-  tasksCl.newTask(t.description, t.completed, t.index);
-});
-
 const todoUl = document.getElementById('todo-ul');
 
-tasksCl.allTasks.forEach((task) => {
-  const liB = document.createElement('li');
-  liB.setAttribute('id', `id${task.taskIndex}`);
-  const x = document.createElement('INPUT');
-  x.setAttribute('type', 'checkbox');
-  liB.appendChild(x);
-  const p = document.createElement('p');
-  p.setAttribute('id', `p${task.taskIndex}`);
-  p.innerHTML = task.taskDesc;
-  liB.appendChild(p);
 
-  const i = document.createElement('i');
-  i.setAttribute('class', 'fa-solid fa-ellipsis-vertical');
-  liB.appendChild(i);
+// tasksCl.allTasks.forEach((task) => {
+//   const liB = document.createElement('li');
+//   liB.setAttribute('id', `id${task.taskIndex}`);
+//   const x = document.createElement('INPUT');
+//   x.setAttribute('type', 'checkbox');
+//   liB.appendChild(x);
+//   const p = document.createElement('p');
+//   p.setAttribute('id', `p${task.taskIndex}`);
+//   p.innerHTML = task.taskDesc;
+//   liB.appendChild(p);
 
-  todoUl.appendChild(liB);
-});
+//   const i = document.createElement('i');
+//   i.setAttribute('class', 'fa-solid fa-ellipsis-vertical');
+//   liB.appendChild(i);
+
+//   todoUl.appendChild(liB);
+// });
 
 const liClear = document.createElement('li');
 liClear.setAttribute('id', 'clearList');
@@ -52,3 +31,31 @@ const pClear = document.createElement('p');
 pClear.innerHTML = 'Clear all completed';
 liClear.appendChild(pClear);
 todoUl.appendChild(liClear);
+const clearTodo = document.getElementById('clearList');
+
+const addTask = document.getElementById('add-task');
+const addIcon = document.getElementById('add-icon');
+
+let currentIndex = 0
+addIcon.addEventListener('click', () => {
+  tasksCl.newTask(addTask.value, false, currentIndex);
+
+  const liB = document.createElement('li');
+  liB.setAttribute('id', `id${tasksCl.allTasks[currentIndex].index}`);
+  const x = document.createElement('INPUT');
+  x.setAttribute('type', 'checkbox');
+  liB.appendChild(x);
+  const p = document.createElement('p');
+  p.setAttribute('id', `p${tasksCl.allTasks[currentIndex].index}`);
+  p.innerHTML = tasksCl.allTasks[currentIndex].desc;
+  liB.appendChild(p);
+
+  const i = document.createElement('i');
+  i.setAttribute('class', 'fa-solid fa-ellipsis-vertical');
+  liB.appendChild(i);
+
+  todoUl.insertBefore(liB, clearTodo);
+
+  currentIndex++;
+})
+
