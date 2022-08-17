8eb3f951fcfc44441a55e7b2e86bdf4332d8854e
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-17 22:47:45 +0100

    Add test for removing task

--- src/modules/todo.test.js
+++ src/modules/todo.test.js
@@ -2,6 +2,8 @@
 
 import addHtml from './AddHtml.js'
 import Tasks from './Tasks.js';
+import remoHtml from "./RemoHtml.js";
+
 
 describe('Add and Remove Task', () => {
   test('Add a new task', () => {
@@ -15,4 +17,15 @@ describe('Add and Remove Task', () => {
     const list = document.querySelectorAll('.todo-item');
     expect(list).toHaveLength(1);
   });
+  
+  test("Remove a task", () => {
+    document.body.innerHTML =
+      "<li id='id1' class='todo-item'><div class='left-li' id='ld1'><input type='checkbox' id='chid1' class='checkbox-item'><p id='p1' class='p-task'>New item</p></div><i class='menu-display fa-solid fa-ellipsis-vertical' id='1'></i></li>";
+    const tasksCl = new Tasks();
+    remoHtml("1", tasksCl);
+
+    const list = document.querySelectorAll(".todo-item");
+    expect(list).toHaveLength(0);
+  });
+
 })
