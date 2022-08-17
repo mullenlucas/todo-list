f539dc3599eef190de6873627c64ed551a1993eb
mullenslucas <mullenlucas@gmail.com>
2022-08-17 18:50:12 -0300

    fix linter errors

--- src/modules/todo.test.js
+++ src/modules/todo.test.js
@@ -1,9 +1,8 @@
 /** * @jest-environment jsdom */
 
-import addHtml from './AddHtml.js'
+import addHtml from './AddHtml.js';
 import Tasks from './Tasks.js';
-import remoHtml from "./RemoHtml.js";
-
+import remoHtml from './RemoHtml.js';
 
 describe('Add and Remove Task', () => {
   test('Add a new task', () => {
@@ -13,19 +12,17 @@ describe('Add and Remove Task', () => {
     const tasksCl = new Tasks();
 
     todoUl.insertBefore(addHtml('holis', 1, 'hola', false, 1, tasksCl), clearTodo);
-  
+
     const list = document.querySelectorAll('.todo-item');
     expect(list).toHaveLength(1);
   });
-  
-  test("Remove a task", () => {
-    document.body.innerHTML =
-      "<li id='id1' class='todo-item'><div class='left-li' id='ld1'><input type='checkbox' id='chid1' class='checkbox-item'><p id='p1' class='p-task'>New item</p></div><i class='menu-display fa-solid fa-ellipsis-vertical' id='1'></i></li>";
-    const tasksCl = new Tasks();
-    remoHtml("1", tasksCl);
 
-    const list = document.querySelectorAll(".todo-item");
+  test('Remove a task', () => {
+    document.body.innerHTML = "<li id='id1' class='todo-item'><div class='left-li' id='ld1'><input type='checkbox' id='chid1' class='checkbox-item'><p id='p1' class='p-task'>New item</p></div><i class='menu-display fa-solid fa-ellipsis-vertical' id='1'></i></li>";
+    const tasksCl = new Tasks();
+    remoHtml('1', tasksCl);
+
+    const list = document.querySelectorAll('.todo-item');
     expect(list).toHaveLength(0);
   });
-
-})
+});
