8de3d76d261ccc8b27472298f3f234f54df24a1a
mullenslucas <mullenlucas@gmail.com>
2022-08-19 18:57:23 -0300

    fix linter errors

--- src/modules/tests/clearedit.test.js
+++ src/modules/tests/clearedit.test.js
@@ -2,12 +2,11 @@
 
 import addHtml from '../AddHtml.js';
 import Tasks from '../Tasks.js';
-import completedFunc from "../completedTasks.js";
-
+import completedFunc from '../completedTasks.js';
 
 describe('Edit, remove and clear-all Task operations', () => {
   test('Editing a task', () => {
-    document.body.innerHTML = "<ul id='todo-ul'><li id='id1' class='todo-item'><div class='left-li' id='ld1'><input type='checkbox' id='chid1' class='checkbox-item'><input type='text' class='editing-task'></div><i class='menu-display fa-solid fa-ellipsis-vertical' id='1'></i></li></ul>"
+    document.body.innerHTML = "<ul id='todo-ul'><li id='id1' class='todo-item'><div class='left-li' id='ld1'><input type='checkbox' id='chid1' class='checkbox-item'><input type='text' class='editing-task'></div><i class='menu-display fa-solid fa-ellipsis-vertical' id='1'></i></li></ul>";
     const todoUl = document.getElementById('todo-ul');
     const clearTodo = document.getElementById('clearList');
     const tasksCl = new Tasks();
@@ -42,7 +41,7 @@ describe('Edit, remove and clear-all Task operations', () => {
     const ediTask = document.querySelectorAll('.editing-task');
     changeDesc(ediTask, tasks);
     expect(ediTask.value).toBe('Clear-time');
-    });
+  });
 
   test('Update a completion value', () => {
     const tasks = [
@@ -69,33 +68,32 @@ describe('Edit, remove and clear-all Task operations', () => {
 
     tasks.forEach((t) => {
       changeCompletion(t);
-    })
+    });
 
     expect(tasks[0].bval).toBe(true);
     expect(tasks[1].bval).toBe(false);
     expect(tasks[2].bval).toBe(true);
   });
 
-  test("Clear all completed values", () => {
-    document.body.innerHTML =
-      "<ul id='todo-ul'></i></li><li id='clearList'><p>Clear all completed</p></li></ul>";
-    const todoUl = document.getElementById("todo-ul");
-    const clearTodo = document.getElementById("clearList");
+  test('Clear all completed values', () => {
+    document.body.innerHTML = "<ul id='todo-ul'></i></li><li id='clearList'><p>Clear all completed</p></li></ul>";
+    const todoUl = document.getElementById('todo-ul');
+    const clearTodo = document.getElementById('clearList');
     const tasksCl = new Tasks();
 
     const tasks = [
       {
-        desc: "Clear-space",
+        desc: 'Clear-space',
         bval: false,
         index: 1,
       },
       {
-        desc: "Test-task",
+        desc: 'Test-task',
         bval: true,
         index: 2,
       },
       {
-        desc: "Editable-task",
+        desc: 'Editable-task',
         bval: false,
         index: 3,
       },
@@ -104,14 +102,13 @@ describe('Edit, remove and clear-all Task operations', () => {
     tasks.forEach((sV) => {
       todoUl.insertBefore(
         addHtml(sV.desc, sV.index, sV.desc, sV.bval, sV.index, tasksCl),
-        clearTodo
+        clearTodo,
       );
     });
 
     completedFunc(tasksCl);
 
-    const list = document.querySelectorAll(".todo-item");
+    const list = document.querySelectorAll('.todo-item');
     expect(list).toHaveLength(2);
   });
-
 });
